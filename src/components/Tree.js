49045a592e46796ec85112720f7a8a395d764126
P. Michael Holland <maikeru.horando@gmail.com>
2018-02-27 21:12:27 +0100

    Use TreeNode componenents for everything

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import css from '../styles/Tree.css';
+import TreeNode from './TreeNode';
 
 export default class Tree extends React.Component {
-  constructor() {
-    super();
-    this.state = { parentExpanded: false };
-    this.handleParentClick = this.handleParentClick.bind(this);
-  }
-
-  handleParentClick(e) {
-    this.setState(prevState => ({ parentExpanded: !prevState.parentExpanded }));
-  }
-
   render() {
-    const treeParentClass = this.state.parentExpanded
-      ? 'tree-parent__expanded'
-      : 'tree-parent';
-    const hideChildren = !this.state.parentExpanded;
-    const treeChildClass = hideChildren ? 'tree-child__hidden' : 'tree-child';
+    const children = [
+      {name: 'child1', children: [
+        {name: 'grandchild1'},
+        {name: 'grandchild2'},
+      ]},
+      {name: 'child2'},
+      {name: 'child3'},
+    ];
     return (
-      <div className={treeParentClass} onClick={this.handleParentClick}>
-        Parent
-        <div className={treeChildClass}>Child1</div>
-        <div className={treeChildClass}>Child2</div>
-        <div className={treeChildClass}>Child3</div>
+      <div className="treeContainer">
+        <TreeNode name='parent' children={children} hidden={false} />
       </div>
     );
   }
---
+++ src/components/TreeNode.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class TreeNode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+      hidden: props.hidden
+    };
+    this.handleParentClick = this.handleParentClick.bind(this);
+  }
+
+  handleParentClick(e) {
+    this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
+  }
+
+  render() {
+    const { children, hidden } = this.props;
+
+    let nodeClass = 'tree-node';
+    if (hidden) {
+      nodeClass = `${nodeClass}__hidden`;
+    } else if (this.state.isExpanded) {
+      nodeClass = `${nodeClass}__expanded`;
+    }
+
+    const hideChildren = !this.state.isExpanded;
+    return (
+      <div className="treeWrapper">
+        <div className={nodeClass} onClick={this.handleParentClick}>
+          {this.props.name}
+        </div>
+        {children &&
+          children.map(child => (
+            <TreeNode
+              name={child.name}
+              hidden={hideChildren}
+              hideChildren={hideChildren}
+              children={child.children ? child.children : []}
+            />
+          ))}
+      </div>
+    );
+  }
+}
+
+TreeNode.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+TreeNode.defaultProps = {
+  hidden: false
+};
